6ac6a50e8a37021fb3774e11c3da343edca8d3e4
Arthur Fritz Santiago <arthur.santiago@db1.com.br>
2019-08-30 08:01:49 -0300

    Include Secret Token GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'meetups';
+  gitTest = environment.GITHUB_SECRET;
+
+  ngOnInit(): void {
+    
+  }
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  GITHUB_SECRET: "GITHUB_SECRET_KEY"
 };
